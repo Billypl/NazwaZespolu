ce5b01ce62d25b3f58a0c2a3d61afb20a140ed34
Michał Węsiora <michal_szkola20@wp.pl>
2024-11-28 13:09:14 +0100

    NZ: selenium(fix) - added ordering products testing (points 5,6,7,8)

--- tests/selenium_tests.py
+++ tests/selenium_tests.py
@@ -12,6 +12,8 @@ import time
 TIMEOUT_TIME_SEC = 3
 
 # Class names
+FIRST_CATEGORY_ID = "category-6"
+SECOND_CATEGORY_ID = "category-9"
 CART_COUNT_CLASSNAME = "cart-products-count"
 PRODUCT_LINK_CLASSNAME = "thumbnail.product-thumbnail"
 PRODUCT_LINK_CLASSNAME_XPATH = "thumbnail product-thumbnail"
@@ -24,7 +26,7 @@ PRODUCT_QUANTITY_CLASSNAME_XPATH = "js-cart-line-product-quantity form-control"
 DELETE_PRODUCT_LINK_CLASSNAME_XPATH = "remove-from-cart"
 LOGIN_LINK_CLASSNAME = "user-info"
 REGISTER_LINK_CLASSNAME = "no-account"
-REGISTER_MALE_GENDER_ID = "field-id_gender-1"
+REGISTER_GENDER_ID = "field-id_gender-1"
 REGISTER_NAME_INPUT_ID = "field-firstname"
 REGISTER_SURNAME_INPUT_ID = "field-lastname"
 REGISTER_EMAIL_INPUT_ID = "field-email"
@@ -36,15 +38,33 @@ REGISTER_CHECKBOX3_NAME = "newsletter"
 REGISTER_CHECKBOX4_NAME = "psgdpr"
 REGISTER_SUBMIT_BUTTON_CLASSNAME = "btn.btn-primary.form-control-submit.float-xs-right"
 LOGOUT_LINK_CLASSNAME = "logout.hidden-sm-down"
+GO_TO_CHECKOUT_LINK_CLASSNAME = "checkout.cart-detailed-actions.js-cart-detailed-actions.card-block"
+CHECKOUT_ADDRESS_ALIAS_INPUT_ID = "field-alias"
+CHECKOUT_ADDRESS_COMPANY_INPUT_ID = "field-company"
+CHECKOUT_ADDRESS_NIP_INPUT_ID = "field-vat_number"
+CHECKOUT_ADDRESS_ADDRESS_INPUT_ID = "field-address1"
+CHECKOUT_ADDRESS_ADDRESS_ADD_INFO_INPUT_ID = "field-address2"
+CHECKOUT_ADDRESS_POSTCODE_INPUT_ID = "field-postcode"
+CHECKOUT_ADDRESS_CITY_INPUT_ID = "field-city"
+CHECKOUT_ADDRESS_PHONE_INPUT_ID = "field-phone"
+CHECKOUT_ADDRESS_CONTINUE_BUTTON_NAME = "confirm-addresses"
+CHECKOUT_STEP_COMPLETED_CLASSNAME = "checkout-step.-reachable.-complete.-clickable"
+CHECKOUT_DELIVERY_CARRIER_ID = "delivery_option_2"
+CHECKOUT_DELIVERY_INFO_ID = "delivery_message"
+CHECKOUT_DELIVERY_CONTINUE_BUTTON_NAME = "confirmDeliveryOption"
+CHECKOUT_PAYMENT_METHOD_ID = "payment-option-3"
+CHECKOUT_PAYMENT_ACCEPT_TERMS_CHECKBOX_ID = "conditions_to_approve[terms-and-conditions]"
+CHECKOUT_PAYMENT_SUBMIT_BUTTON_CLASSNAME = "btn.btn-primary.center-block"
+ORDER_REFERENCE_VALUE_ID = "order-reference-value"
 
 
 # Add 10 products (with random quantity) from 2 categories to cart
 def products_to_cart_test(total_cart_quantity):
-    total_cart_quantity[0] = add_category_products_to_cart("category-6")
-    total_cart_quantity[0] += add_category_products_to_cart("category-9")
+    total_cart_quantity[0] = add_category_products_to_cart(FIRST_CATEGORY_ID)
+    total_cart_quantity[0] += add_category_products_to_cart(SECOND_CATEGORY_ID)
 
     check_cart_count_number(total_cart_quantity)
-    print("TEST - Adding 10 products from 2 categories to cart - has completed successfully!")
+    print("TEST - Adding 10 products from 2 categories to cart (1) - has completed successfully!")
 
 def add_category_products_to_cart(category_id):
     cart_quantity = 0
@@ -101,7 +121,7 @@ def search_and_add_product_to_cart_test(search_text, total_cart_quantity):
     close_cart_pop_up()
 
     check_cart_count_number(total_cart_quantity)
-    print("TEST - Searching and adding random product to cart - has completed successfully!")
+    print("TEST - Searching and adding random product to cart (2) - has completed successfully!")
 
 def close_cart_pop_up():
     WebDriverWait(driver, TIMEOUT_TIME_SEC).until(
@@ -121,11 +141,7 @@ def check_cart_count_number(total_cart_quantity):
 
 # Delete 3 products from cart
 def delete_products_from_cart_test(total_cart_quantity):
-    WebDriverWait(driver, TIMEOUT_TIME_SEC).until(
-        EC.presence_of_element_located((By.CLASS_NAME, CART_LINK_CLASSNAME))
-    )
-    cart_link = driver.find_element(By.CLASS_NAME, CART_LINK_CLASSNAME)
-    cart_link.click()
+    go_to_cart()
 
     for i in range(1,4):
         WebDriverWait(driver, TIMEOUT_TIME_SEC).until(
@@ -138,7 +154,7 @@ def delete_products_from_cart_test(total_cart_quantity):
 
     driver.refresh()
     check_cart_count_number(total_cart_quantity)
-    print("TEST - Deleting 3 products from cart - has completed successfully!")
+    print("TEST - Deleting 3 products from cart (3) - has completed successfully!")
 
 # Register new user
 def register_test():
@@ -155,10 +171,10 @@ def register_test():
     register_link.click()
 
     WebDriverWait(driver, TIMEOUT_TIME_SEC).until(
-        EC.presence_of_element_located((By.ID, REGISTER_MALE_GENDER_ID))
+        EC.presence_of_element_located((By.ID, REGISTER_GENDER_ID))
     )
-    male_radio_button = driver.find_element(By.ID, REGISTER_MALE_GENDER_ID)
-    male_radio_button.click()
+    gender_radio_button = driver.find_element(By.ID, REGISTER_GENDER_ID)
+    gender_radio_button.click()
     name_input = driver.find_element(By.ID, REGISTER_NAME_INPUT_ID)
     name_input.send_keys("Jan")
     surname_input = driver.find_element(By.ID, REGISTER_SURNAME_INPUT_ID)
@@ -184,7 +200,86 @@ def register_test():
         EC.presence_of_element_located((By.CLASS_NAME, LOGOUT_LINK_CLASSNAME))
     )
 
-    print("TEST - Registering new account - has completed successfully!")
+    print("TEST - Registering new account (4) - has completed successfully!")
+
+def go_to_cart():
+    WebDriverWait(driver, TIMEOUT_TIME_SEC).until(
+        EC.presence_of_element_located((By.CLASS_NAME, CART_LINK_CLASSNAME))
+    )
+    cart_link = driver.find_element(By.CLASS_NAME, CART_LINK_CLASSNAME)
+    cart_link.click()
+
+# Go to cart and make an order for products in cart
+def make_order_test():
+    go_to_cart()
+
+    WebDriverWait(driver, TIMEOUT_TIME_SEC).until(
+        EC.presence_of_element_located((By.CLASS_NAME, GO_TO_CHECKOUT_LINK_CLASSNAME))
+    )
+    checkout_link = driver.find_element(By.CLASS_NAME, GO_TO_CHECKOUT_LINK_CLASSNAME)
+    checkout_link.click()
+
+    WebDriverWait(driver, TIMEOUT_TIME_SEC).until(
+        EC.presence_of_element_located((By.ID, CHECKOUT_ADDRESS_ALIAS_INPUT_ID))
+    )
+    alias_input = driver.find_element(By.ID, CHECKOUT_ADDRESS_ALIAS_INPUT_ID)
+    alias_input.send_keys("Jasiek2r")
+    company_input = driver.find_element(By.ID, CHECKOUT_ADDRESS_COMPANY_INPUT_ID)
+    company_input.send_keys("Politechnika Gdańska")
+    nip_input = driver.find_element(By.ID, CHECKOUT_ADDRESS_NIP_INPUT_ID)
+    nip_input.send_keys("1111111111")
+    address_input = driver.find_element(By.ID, CHECKOUT_ADDRESS_ADDRESS_INPUT_ID)
+    address_input.send_keys("Gabriela Narutowicza 11/12")
+    address_add_info_input = driver.find_element(By.ID, CHECKOUT_ADDRESS_ADDRESS_ADD_INFO_INPUT_ID)
+    address_add_info_input.send_keys("Nowe Eti")
+    postcode_input = driver.find_element(By.ID, CHECKOUT_ADDRESS_POSTCODE_INPUT_ID)
+    postcode_input.send_keys("80-222")
+    city_input = driver.find_element(By.ID, CHECKOUT_ADDRESS_CITY_INPUT_ID)
+    city_input.send_keys("Gdańsk")
+    phone_input = driver.find_element(By.ID, CHECKOUT_ADDRESS_PHONE_INPUT_ID)
+    phone_input.send_keys("123456789")
+    continue_button = driver.find_element(By.NAME, CHECKOUT_ADDRESS_CONTINUE_BUTTON_NAME)
+    continue_button.click()
+
+    complete_steps = driver.find_elements(By.CLASS_NAME, CHECKOUT_STEP_COMPLETED_CLASSNAME)
+    assert len(complete_steps) == 2, f"Expected 2 completed steps, but got {len(complete_steps)}" 
+
+    print("TEST - Making an order (5) - has completed successfully!")
+
+# Choose one of two possible shipping carriers
+def choose_delivery_test():
+    WebDriverWait(driver, TIMEOUT_TIME_SEC).until(
+        EC.presence_of_element_located((By.ID, CHECKOUT_DELIVERY_CARRIER_ID))
+    )
+    delivery_carrier_radio_button = driver.find_element(By.ID, CHECKOUT_DELIVERY_CARRIER_ID)
+    delivery_carrier_radio_button.click()
+    delivery_info_input = driver.find_element(By.ID, CHECKOUT_DELIVERY_INFO_ID)
+    delivery_info_input.send_keys("leleleel")
+    continue_button = driver.find_element(By.NAME, CHECKOUT_DELIVERY_CONTINUE_BUTTON_NAME)
+    continue_button.click()
+
+    complete_steps = driver.find_elements(By.CLASS_NAME, CHECKOUT_STEP_COMPLETED_CLASSNAME)
+    assert len(complete_steps) == 3, f"Expected 3 completed steps, but got {len(complete_steps)}"
+
+    print("TEST - Choosing shipping carrier (7) - has completed successfully!")
+
+# Choose payment on delivery method and submit the order
+def choose_payment_method_and_submit_test():
+    WebDriverWait(driver, TIMEOUT_TIME_SEC).until(
+        EC.presence_of_element_located((By.ID, CHECKOUT_PAYMENT_METHOD_ID))
+    )
+    payment_method_radio_button = driver.find_element(By.ID, CHECKOUT_PAYMENT_METHOD_ID)
+    payment_method_radio_button.click()
+    accept_terms_checkbox = driver.find_element(By.ID, CHECKOUT_PAYMENT_ACCEPT_TERMS_CHECKBOX_ID)
+    accept_terms_checkbox.click()
+    submit_button = driver.find_element(By.CLASS_NAME, CHECKOUT_PAYMENT_SUBMIT_BUTTON_CLASSNAME)
+    submit_button.click()
+
+    WebDriverWait(driver, TIMEOUT_TIME_SEC).until(
+        EC.presence_of_element_located((By.ID, ORDER_REFERENCE_VALUE_ID))
+    )
+
+    print("TEST - Choosing payment on delivery method and submitting and order (6,8) - has completed successfully!")
 
 
 # Create Google Chrome webdriver to perform test actions
@@ -199,6 +294,9 @@ products_to_cart_test(total_cart_quantity)
 search_and_add_product_to_cart_test("czarny", total_cart_quantity)
 delete_products_from_cart_test(total_cart_quantity)
 register_test()
+make_order_test()
+choose_delivery_test()
+choose_payment_method_and_submit_test()
 time.sleep(7)
 
 print("All tests have completed successfully!")
