8c612332d8ec3e0dbe25f8040ecd327c716619f7
Billypl <kriper01@wp.pl>
2024-12-07 17:45:19 +0100

    NZ: products(fix) - add script for auto googledrive images download and restore original db state (before products upload)

---
+++ integration/local/restore_all.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DUMP_DIR="../../prestashop/dbdump"
+DUMP_FILE="dump.sql"
+DB_CONTAINER_NAME="some-mysql"
+DB_NAME="prestashop"
+DB_PASSWORD="admin" # DON'T store password like that - its fine for development
+GOOGLE_DRIVE_ZIP_LINK="https://drive.google.com/file/d/177JtRtuWCF03i5keMyCckc_c48pcA0qh/view?usp=drive_link"
+
+# # NAMES WITH TIMESTAMPS RESTORATION
+# echo "Please enter the dump file name (e.g., dump_22-30_17-11-24.sql):"
+# read DUMP_FILE
+
+DUMP_FILE_PATH="$DUMP_DIR/$DUMP_FILE"
+
+if [ ! -f "$DUMP_FILE_PATH" ]; then
+  echo "File not found!"
+  exit 1
+fi
+
+echo "Restoring database..."
+docker exec -i $DB_CONTAINER_NAME mariadb $DB_NAME --user=root -p$DB_PASSWORD < "$DUMP_FILE_PATH"
+echo "Database restored"
+
+echo "Restoring images..."
+file_id=$(echo $GOOGLE_DRIVE_ZIP_LINK | awk -F'/d/|/view' '{print $2}')
+wget -O p.zip "https://drive.usercontent.google.com/download?export=download&confirm=t&id=$file_id"
+rm -rf "../../prestashop/src/img/p"
+unzip p.zip -d "../../prestashop/src/img/"
+rm p.zip
+
+echo "Images restored"
